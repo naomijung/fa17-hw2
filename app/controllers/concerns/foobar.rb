f4cb7feed742a8947772bf238588011d20419cf4
Naomi Jung <naomi.jung@berkeley.edu>
2017-09-27 00:30:48 -0700

    hw2 submission

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,13 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  # foo = Foobar.new "baz"
+  # @baz = foo.bar :cat, sat: :dat, dat: :sat
+  # return: "catbazdat"
   def initialize(baz)
+    @baz = baz
+  end
+
+  def bar(str, hash)
+    @baz = str.to_s() + @baz + hash.keys.min.to_s()
   end
 end
